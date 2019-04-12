f44ebcff3f72ca18ba0ffca4ad09d29858058eef
Dylan-Morrissey <DylanMorrissey2010@gmail.com>
2019-04-12 12:18:50 +0100

    Datatypes and Variables in Javascript

--- comments.js
+++ comments.js
@@ -1,9 +1,10 @@
-// This is how to do a single line comment 
+// This is how to do a single line comment
+
+var number = 5; // you can also do an inline comment like so
 
 /*
-
 This is 
-how you can do a 
-multi line comment.
+how you can do
+a multi line comment.
 */
 
---
+++ datatypesVariables.js
@@ -0,0 +1,31 @@
+// JavaScript has 7 diffrent datatypes.
+
+/*
+undefined something that has not been defined
+null set something to nothing.
+boolean something is true or false.
+string is text
+number is a number 
+symbol immutable primmitve value
+object stores key value pairs
+*/
+
+
+var myName = "Dylan" // String
+myName = 8 // number 
+
+let hisName = "John" 
+
+const pi = 3.14 // never change it!!
+
+// Assigning Variables
+
+var a; // declaring variable a 
+console.log(a) //print to console
+var b = 2; // declaring variable b and assigning it 2
+
+a = 7; //assigns 7 to a
+
+b = a; // contents of a to b 
+
+console.log(a) //print to console
