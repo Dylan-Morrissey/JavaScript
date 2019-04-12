ca77f4ba37cb92a39a7a4d50293dbe47b6495ef8
Dylan-Morrissey <dylanmorrissey2010@gmail.com>
2019-04-12 12:43:09 +0100

    Looked at all basic operations and some shortcuts in JS.

---
+++ operation.js
@@ -0,0 +1,44 @@
+var sum = 5 + 5; // addition
+console.log(sum);
+
+var diffrence = sum - 5; // subtraction
+console.log(diffrence);
+
+var product = 2 * 5; // multiplication
+console.log(product);
+
+var division = product / 2; // division
+console.log(division);
+
+var increase = 100;
+console.log(increase);
+increase++; // adds 1 to increase
+console.log(increase);
+
+var decrease = 100 
+console.log(decrease);
+decrease--;
+console.log(decrease)
+
+var decimal = 5.6; // decimal numbers
+
+var decimalmultiply = decimal * decimal;
+console.log(decimalmultiply)
+
+var remainder;
+remainder = 11 % 3; // remainder
+console.log(remainder);
+
+// Some Shortcuts
+var a = 12;
+// a  = a +12;
+a += 12;
+
+// a = a - 2;
+a -= 2;
+
+// a = a * 5
+a += 5;
+
+// a = a / 12
+a /= 12;
